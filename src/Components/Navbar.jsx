10d631d6d593e92c4a1194a32941a368ad8cbdea
Igor Lukashev <motrdevua@gmail.com>
2020-09-05 21:44:59 +0300

    lesson 12

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <a href="item 1">Profile</a>
+        </li>
+        <li>
+          <a href="item 2">Messages</a>
+        </li>
+        <li>
+          <a href="item 3">News</a>
+        </li>
+        <li>
+          <a href="item 4">Settings</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
