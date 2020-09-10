b6d6bbde230ed3c855648b6b9d17a5eea6a87882
Igor Lukashev <motrdevua@gmail.com>
2020-09-10 08:06:42 +0300

    a few changes

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -6,11 +6,26 @@ const Dialogs = () => (
     <h2 className={s.title}>Dialogs</h2>
     <div className={s.dialogs__wrapper}>
       <div className={s.dialogs__items}>
-        <div className={s.dialog}>John</div>
-        <div className={s.dialog}>Jack</div>
-        <div className={s.dialog}>Anna</div>
+        <div className={s.dialog}>
+          <span className={s.dialog__name}>John</span>
+        </div>
+        <div className={s.dialog}>
+          <span className={s.dialog__name}>Jack</span>
+        </div>
+        <div className={s.dialog}>
+          <span className={s.dialog__name}>Anna</span>
+        </div>
       </div>
       <div className={s.messages}>
+        <div className={s.message}>
+          <p className={s.message__text}>some message ...</p>
+        </div>
+        <div className={s.message}>
+          <p className={s.message__text}>some message ...</p>
+        </div>
+        <div className={s.message}>
+          <p className={s.message__text}>some message ...</p>
+        </div>
         <div className={s.message}>
           <p className={s.message__text}>some message ...</p>
         </div>
